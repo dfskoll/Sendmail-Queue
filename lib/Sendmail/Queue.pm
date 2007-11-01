14f8179139c58e189e37fc974d4487954bbeb026
David F. Skoll <dfs@roaringpenguin.com>
2007-11-01 16:14:39 -0400

    Starting on Sendmail::Queue Perl module.

---
+++ lib/Sendmail/Queue.pm
@@ -0,0 +1,100 @@
+package Sendmail::Queue;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Sendmail::Queue - The great new Sendmail::Queue!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Sendmail::Queue;
+
+    my $foo = Sendmail::Queue->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+David F. Skoll, C<< <dfs at roaringpenguin.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-sendmail-queue at rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Sendmail-Queue>.
+I will be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Sendmail::Queue
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Sendmail-Queue>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Sendmail-Queue>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Sendmail-Queue>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Sendmail-Queue>
+
+=back
+
+=head1 ACKNOWLEDGEMENTS
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2007 David F. Skoll, all rights reserved.
+
+This program is released under the following license: restrictive
+
+=cut
+
+1; # End of Sendmail::Queue
