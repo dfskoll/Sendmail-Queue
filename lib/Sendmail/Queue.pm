d2387df51ab763ddbfb64fcb3247dd46af0f24fa
Dave O'Neill <dmo@roaringpenguin.com>
2010-12-30 14:44:00 -0500

    Sendmail::Queue is GPLv2

--- lib/Sendmail/Queue.pm
+++ lib/Sendmail/Queue.pm
@@ -526,3 +526,8 @@ Dave O'Neill, C<< <support at roaringpenguin.com> >>
 =head1 LICENCE AND COPYRIGHT
 
 Copyright (c) 2007 Roaring Penguin Software, Inc.  All rights reserved.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
